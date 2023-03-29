584c8a58556ff5662fa69a65ed91be5b4b0624ab
lesia2022 <lesia.bezukh22@gmail.com>
2023-03-29 18:57:59 +0300

    Npm

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict'
+const sum = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+
+console.log(sum(2, 3)); // 5
+console.log(subtract(5, 2)); // 3
+console.log(multiply(5, 2)); // 10
+
